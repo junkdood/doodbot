4282b0bb918b8ed0918dcc96fda5974281a5d2d5
junkdood <1354784375@qq.com>
2022-04-12 15:45:37 +0800

    joycon failed

--- src/joycon.py
+++ src/joycon.py
@@ -5,6 +5,19 @@ from doodbot.msg import Gamepad
 
 from pyjoycon import JoyCon, get_R_id
 
+import hid
+
+for device_dict in hid.enumerate():
+    keys = list(device_dict.keys())
+    keys.sort()
+    for key in keys:
+        print("%s : %s" % (key, device_dict[key]))
+    print()
+
+devices = hid.enumerate()
+print(devices)
+
+print(get_R_id())
 joycon_id = get_R_id()
 joycon = JoyCon(*joycon_id)
 
